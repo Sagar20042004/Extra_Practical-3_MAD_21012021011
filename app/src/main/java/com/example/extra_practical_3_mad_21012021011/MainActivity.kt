c0af60584bc390e664e1d8625a5747545d8163b7
student <student@gnu.ac.in>
2023-08-04 08:53:55 +0530

    Initial commit

---
+++ app/src/main/java/com/example/extra_practical_3_mad_21012021011/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.extra_practical_3_mad_21012021011
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Extra_Practical-3_MAD_21012021011"
+include(":app")
